605a2226e54e891f20968407048d03ebb652de1d
ope001 <ope001>
2016-10-06 16:41:02 +0900

    Create DB && Util

---
+++ app/src/main/java/com/happypiebinliu/happypieweather/DBUtil/HappyPieWeatherOpenHelper.java
@@ -0,0 +1,58 @@
+package com.happypiebinliu.happypieweather.DBUtil;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by ope001 on 2016/10/06.
+ */
+
+public class HappyPieWeatherOpenHelper extends SQLiteOpenHelper {
+
+
+    /**
+     * Created [Province] table
+     */
+    public static final String CREATE_PROVINCE_TABLE = "create table Province ("
+            + "id integer primary key autoincrement,"
+            + "province_name text,"
+            + "province_code text)";
+
+    /**
+     * Created [City] table
+     */
+    public static final String CREATE_CITY_TABLE = "create table City ("
+            + "id integer primary key autoincrement,"
+            + "city_name text,"
+            + "city_code text,"
+            + "province_id integer)";
+
+    /**
+     * Created [County] table
+     */
+    public static final String CREATE_COUNTY_TABLE = "create table County ("
+            + "id integer primary key autoincrement,"
+            + "county_name text,"
+            + "county_code text"
+            + "city_id integer)";
+
+    public HappyPieWeatherOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        // onCreate method : DB table
+        sqLiteDatabase.execSQL(CREATE_PROVINCE_TABLE);
+        sqLiteDatabase.execSQL(CREATE_CITY_TABLE);
+        sqLiteDatabase.execSQL(CREATE_COUNTY_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
---
+++ app/src/main/java/com/happypiebinliu/happypieweather/Model/Province.java
@@ -0,0 +1,36 @@
+package com.happypiebinliu.happypieweather.Model;
+
+/**
+ * Created by ope001 on 2016/10/06.
+ */
+
+public class Province {
+
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
